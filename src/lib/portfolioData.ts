d27b1c9ed617463a97e81b03684032ca04632ec1
Lo3wer <leozhang226@gmail.com>
2026-07-29 14:10:56 -0700

    fixed image links and risc processor link

--- src/lib/portfolioData.ts
+++ src/lib/portfolioData.ts
@@ -2,7 +2,7 @@ export const portfolioItems = [
     { 
       title: "Rust Lox Treewalk Interpreter", 
       description: "Jlox inspired treewalk interpreter implemented in Rust without external libraries", 
-      imageUrl: "/images/crafting-interpreters.png", 
+      imageUrl: "/images/projects/crafting-interpreters.png", 
       link: "https://github.com/Lo3wer/jlox-treewalk-interpreter-rust", 
       category: "Software", 
       date: "July 2026"
@@ -11,7 +11,7 @@ export const portfolioItems = [
     { 
       title: "Chip-8 Emulator", 
       description: "IBM Chip-8 emulator written using C, and displayed with SDL", 
-      imageUrl: "/images/chip-8_ibm_logo.png", 
+      imageUrl: "/images/projects/chip-8_ibm_logo.png", 
       link: "https://github.com/Lo3wer/chip-8", 
       category: "Software", 
       date: "December 2025" 
@@ -19,7 +19,7 @@ export const portfolioItems = [
     { 
       title: "NewsMapper", 
       description: "A way to view past and present events on an interactive map. NWHacks 2025", 
-      imageUrl: "/images/newsmapper.png", 
+      imageUrl: "/images/projects/newsmapper.png", 
       link: "https://devpost.com/software/news-mapper", 
       category: "Software", 
       date: "January 2025" 
@@ -27,15 +27,15 @@ export const portfolioItems = [
     { 
       title: "Pantry Patrol", 
       description: "Track expiry dates and get recipe suggestions. CPEN221A Group Project", 
-      imageUrl: "/images/pantry_patrol_otherpage.png", 
+      imageUrl: "/images/projects/pantry_patrol_otherpage.png", 
       link: "https://github.com/u759/PantryPatrol", 
       category: "Software", 
       date: "December 2024" 
     },
     { title: "Verilog RISC Processor", 
       description: "16-bit RISC processor on DE1-SOC FPGA. CPEN211 Lab", 
-      imageUrl: "/images/de1soc.jpg", 
-      link: "/project3", 
+      imageUrl: "/images/projects/de1soc.jpg", 
+      link: "https://github.com/Lo3wer/cpen211-risc-cpu", 
       category: "Hardware", 
       date: "January 2024" 
     },
